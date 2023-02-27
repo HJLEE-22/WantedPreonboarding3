acb76097db549bd8fa789e91211e19d0af2a1d19
Hyungju LEE <hyungjuice@naver.com>
2023-02-27 15:30:43 +0900

    first commit

---
+++ WantedPreonboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreonboarding
+//
+//  Created by 이형주 on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
